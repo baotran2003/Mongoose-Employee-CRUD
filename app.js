75df57763143e60969d6634b8a488240033604cf
Bao Tran <trankhuongbao2003@gmail.com>
2025-02-24 23:24:41 +0700

    Connecting to MongoDB using Mongoose

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+// Connect MongoDB Database
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view-engine", "ejs");
+app.use(express.static("public"));
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log("Server started on port 3000.");
+});
