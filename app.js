8565b9b7d9bca854336bb6e424c04e4fce619dd2
Bao Tran <trankhuongbao2003@gmail.com>
2025-02-26 14:11:54 +0700

    update function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const employeeRoutes = require("./routes/employees");
 
@@ -23,6 +24,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// middleware for method override
+app.use(methodOverride("_method"));
 
 app.use(employeeRoutes);
 
--- routes/employees.js
+++ routes/employees.js
@@ -36,6 +36,7 @@ router.get("/employee", (req, res) => {
         });
 });
 
+// save Employee after Create.
 router.post("/employee/new", (req, res) => {
     let newEmployee = {
         name: req.body.name,
@@ -53,4 +54,38 @@ router.post("/employee/new", (req, res) => {
         });
 });
 
+// updated Employee
+router.get("/edit/:id", (req, res) => {
+    let searchQuery = { _id: req.params.id };
+    Employee.findOne(searchQuery)
+        .then((employee) => {
+            res.render("edit", { employee: employee });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send("Internal Server Error"); // Trả về phản hồi lỗi cho client
+        });
+});
+
+// updated employee button
+router.put("/edit/:id", (req, res) => {
+    let searchQuery = { _id: req.params.id };
+
+    Employee.updateOne(searchQuery, {
+        $set: {
+            name: req.body.name,
+            designation: req.body.designation,
+            salary: req.body.salary,
+        },
+    })
+        .then((employee) => {
+            res.redirect("/");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send("Internal Server Error"); // Trả về phản hồi lỗi cho client
+        });
+});
+
+// delete Employee
 module.exports = router;
